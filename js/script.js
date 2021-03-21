242eb0ee013df1dcc0911d3e552ab0f6815c50cd
tka4nik <tka4nik@gmail.com>
2021-03-21 14:31:28 +0300

    downgrade of images sizes, 7 sec image change timer instead of 10

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var slideshow = new Slideshow({
     backgroundElementId: "bg",
-    tickInterval: 10000,
+    tickInterval: 7000,
     transitionTime: 500,
 });
 slideshow.setImages(['./images/background_1.png','./images/bg_2.jpg','./images/bg_6.png','./images/bg_3.jpg',]);
