1483ef922d2054689e2007c1fcd5d5fb940e5ce1
tka4nik <tka4nik@gmail.com>
2021-03-23 11:36:04 +0300

    new bgs <3 LadyBaggins

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ var slideshow = new Slideshow({
     tickInterval: 7000,
     transitionTime: 500,
 });
-slideshow.setImages(['./images/background_1.jpg','./images/bg_10.jpg','./images/bg_2.jpg','./images/bg_6.png','./images/bg_7.jpg','./images/bg_8.jpg','./images/bg_3.jpg','./images/bg_9.jpg']);
+slideshow.setImages(['./images/bg_12.jpg','./images/bg_7.jpg','./images/bg_10.jpg','./images/bg_2.jpg','./images/bg_6.png','./images/bg_8.jpg','./images/bg_3.jpg', './images/bg_11.jpg', './images/bg_13.jpg', './images/bg_14.jpg', './images/bg_15.jpg', './images/bg_16.jpg']);
 slideshow.run();
